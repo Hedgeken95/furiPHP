24615b771265e2cc8c86fb1766cdc3f4a69c84c0
Hedgeken95 <ken10951128@icloud.com>
2020-10-30 17:02:04 +0900

    phpの自動変換

--- chap1_4_3.php
+++ chap1_4_3.php
@@ -4,4 +4,9 @@ $price = 150;
 var_dump( $msg );
 var_dump( $price );
 var_dump( $msg . $price );
+
+var_dump( "数値を入力せよ" );
+$value = trim( fgets( STDIN ) );
+var_dump( $value );
+var_dump( $value + 10 );
 ?>
